4cacf6d2f716cc5ed5ca8945545793d24d3be68c
Leo Marinho <dev.leomarinho@gmail.com>
2024-11-24 17:38:36 -0300

    adiciona as classes Carrinho e Produto

--- src/model/Carrinho.java
+++ src/model/Carrinho.java
@@ -1,4 +1,33 @@
 package model;
 
+import strategy.DescontoStrategy;
+import strategy.SemDesconto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Carrinho {
+
+    private final List<Produto> produtos = new ArrayList<>();
+    private DescontoStrategy descontoStrategy = new SemDesconto();
+
+    public void adicionarProduto (Produto produto){
+        produtos.add(produto);
+    }
+
+    public void definirDesconto(DescontoStrategy descontoStrategy){
+        this.descontoStrategy = descontoStrategy;
+    }
+
+    public double calcularTotal(){
+        double valorTotal= 0;
+
+        for(Produto produto : produtos){
+            valorTotal += produto.getPreco();
+        }
+
+        return descontoStrategy.aplicarDesconto(valorTotal);
+    }
+
+
 }
--- src/model/Produto.java
+++ src/model/Produto.java
@@ -1,4 +1,19 @@
 package model;
 
 public class Produto {
+    private final String nome;
+    private final double preco;
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
 }
